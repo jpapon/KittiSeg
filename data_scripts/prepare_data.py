ba4da6188f7e1726896cbbe9f9464863b4514f26
Jeremie Papon <jpapon@gmail.com>
2017-03-23 15:58:45 -0700

    Updated data prep script to be adjustable

--- data_scripts/prepare_data.py
+++ data_scripts/prepare_data.py
@@ -17,61 +17,75 @@ import scipy.misc
 import sys
 from random import shuffle
 import zipfile
-
+from glob import glob
 
 import logging
-import utils
 reload(logging)
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     level=logging.DEBUG,
                     stream=sys.stdout)
 
+IMAGE_FOLDER = "depth_image"
+GT_FOLDER = "gt_image"
+DATA_DIR = "../DATA/data_RCTA/carry"
+TRAIN_FILE_OUT = "train.txt"
+VAL_FILE_OUT = "val.txt"
+VAL_PROP = 0.05
 
-def make_val_split(data_folder):
+def make_val_split(file_tuples, data_folder):
     """
     Splits the Images in train and test.
     Assumes a File all.txt in data_folder.
     """
 
-    all_file = "all.txt"
-    train_file = "train.txt"
-    test_file = "val.txt"
-    test_num = 20
 
-    filename = os.path.join(data_folder, all_file)
-    assert os.path.exists(filename), ("File not Found %s"
-                                      % filename)
+    test_num = len (file_tupes) * VAL_PROP
 
-    files = [line for line in open(filename)]
+    shuffle(file_tuples)
 
-    shuffle(files)
+    train = file_tuples[:-test_num]
+    test = file_tuples[-test_num:]
 
-    train = files[:-test_num]
-    test = files[-test_num:]
-
-    train_file = os.path.join(data_folder, train_file)
-    test_file = os.path.join(data_folder, test_file)
+    train_file = os.path.join(data_folder, TRAIN_FILE_OUT)
+    test_file = os.path.join(data_folder, VAL_FILE_OUT)
 
     with open(train_file, 'w') as file:
         for label in train:
-            file.write(label)
+            file.write(label[0] +' ' + label[1]+'\n')
 
     with open(test_file, 'w') as file:
         for label in test:
-            file.write(label)
+            file.write(label[0] +' ' + label[1]+'\n')
 
+def get_file_tuples (data_dir):
+    image_folder = os.path.join(data_dir, 'training',IMAGE_FOLDER)
+    gt_folder = os.path.join(data_dir, 'training',GT_FOLDER)
+
+    image_files = glob(os.path.join(image_folder,'*.png'))
+    image_files = [ os.path.join('training',IMAGE_FOLDER,os.path.basename(image_file)) for image_file in image_files]
+    gt_image_files = glob(os.path.join(gt_folder,'*.png'))
+    gt_image_file_dic = {os.path.basename(gt_file) : os.path.join('training',GT_FOLDER,os.path.basename(gt_file)) for gt_file in gt_image_files}
+    file_tuples = []
+    for image_file in image_files:
+      basename = os.path.basename(image_file)
+      if (basename in gt_image_file_dic):
+        file_tuples.append((image_file, gt_image_file_dic[basename]))
+
+    print ("Found {} matching file pairs".format(len(file_tuples)))
+    return file_tuples
 
 def main():
-    data_dir = utils.cfg.data_dir
-    zip_file = "data_road.zip"
-    zip_file = os.path.join(data_dir, zip_file)
-    if not os.path.exists(zip_file):
-        logging.error("File not found: %s", zip_file)
-        exit(1)
-    zipfile.ZipFile(zip_file, 'r').extractall(data_dir)
 
-    make_val_split(data_dir)
+
+    #zip_file = "data_road.zip"
+    #zip_file = os.path.join(data_dir, zip_file)
+    #if not os.path.exists(zip_file):
+    #    logging.error("File not found: %s", zip_file)
+    #    exit(1)
+    #zipfile.ZipFile(zip_file, 'r').extractall(data_dir)
+    file_tuples = get_file_tuples (DATA_DIR)
+    make_val_split(file_tuples, DATA_DIR)
 
 
 if __name__ == '__main__':
