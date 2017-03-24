21daf36a4759aee33cd8c377a93503c8958ec338
Jeremie Papon <jpapon@gmail.com>
2017-03-24 11:41:32 -0700

    Updated network to have an RCTA depth network outside of the fcn_tensorflow lib

--- encoder/fcn8_vgg_RCTA_depth.py
+++ encoder/fcn8_vgg_RCTA_depth.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow_fcn import fcn8_vgg_depth
+from networks import fcn8_vgg_depth
 
 import tensorflow as tf
 
